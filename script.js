9479cf036ef74ef69e63e6491abd357284c6f954
Abby Wagoner <awagoner05@icloud.com>
2020-07-16 19:44:03 -0500

    Starter code with load and submit event listeners

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 // Write your JavaScript code here!
 
+window.addEventListener("load", function() {
+   let form = document.querySelector("launchForm");
+   form.addEventListener("submit", function(event) {
+     let pilotName = document.querySelector("input[name=pilotName]");
+     let copilotName = document.querySelector("input[name=copilotName]");
+     let fuelLevel = document.querySelector("input[name=fuelLevel]");
+     let cargoMass = document.querySelector("input[name=cargoMass]");
+      if (usernameInput.value === "" || teamName.value === "") {
+         alert("All fields are required!");
+      }
+   });
+ });
+
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
