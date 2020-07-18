1f8eeb0ba1bd1ebf919e9662f4af3f9ce6693f16
Abby Wagoner <awagoner05@icloud.com>
2020-07-17 23:16:10 -0500

    Added shuttle requirements, and fetching planetary data at random

--- script.js
+++ script.js
@@ -2,6 +2,32 @@
 
 window.addEventListener("load", function() {
    let form = document.getElementById("launchForm");
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then(function(json) {
+         let index = Math.floor(Math.random() * json.length);
+         const target = document.getElementById('missionTarget');
+            target.innerHTML = `
+                  <h2>Mission Destination</h2>
+                     <ol>
+                        <li>Name: ${json[index].name}</li>
+                        <li>Diameter: ${json[index].diameter}</li>
+                        <li>Star: ${json[index].star}</li>
+                        <li>Distance from Earth: ${json[index].distance}</li>
+                        <li>Number of Moons: ${json[index].moons}</li>
+                      </ol>
+                     <img src="${json[index].image}">
+                   `;
+      });
+    
+   });  
+
+
+
+
+
+
+
+
    form.addEventListener("submit", function(event) {
      let pilotName = document.querySelector("input[name=pilotName]");
      let copilotName = document.querySelector("input[name=copilotName]");
@@ -14,14 +40,38 @@ window.addEventListener("load", function() {
             event.preventDefault();
       };
       
-      if (isNaN(pilotName.value) || isNaN(copilotName.value) || 
-            typeOf(fuelLevel.value) === Number || typeOf(cargoMass.value) === Number) {
+      if (typeof(pilotName.value) !== 'string' || typeof(copilotName.value) !== 'string' || 
+            isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
                alert("Please enter valid information!")
                event.preventDefault();
             };
          
-      document.querySelector("faultyItems")
-         
+      document.getElementById("pilotStatus").innerHTML = `${pilotName.value} Ready`;
+      document.getElementById("copilotStatus").innerHTML = `${copilotName.value} Ready`;
+      
+      let faultyItems = document.getElementById("faultyItems");
+      let launchStatus = document.getElementById('launchStatus');
+
+      if (fuelLevel.value < 10000) {
+         let fuelStatus = document.getElementById('fuelStatus');
+         faultyItems.style.visibility = 'visible';
+         fuelStatus.innerHTML = 'Fuel level too low for launch!';
+         launchStatus.innerHTML = 'Shuttle not ready for launch';
+         launchStatus.style.color = 'red';
+      } else if (cargoMass.value > 10000) {
+         let cargoStatus = document.getElementById('cargoStatus');
+         faultyItems.style.visibility = 'visible';
+         cargoStatus.innerHTML = 'Cargo mass too high for launch!';
+         launchStatus.innerHTML = 'Shuttle not ready for launch';
+         launchStatus.style.color = 'red';
+      } else {
+         launchStatus.style.color = 'green';
+         launchStatus.innerHTML = 'Shuttle is ready for launch!'
+      }
+      event.preventDefault();    
+   
+   
+   
    });
  });
 
